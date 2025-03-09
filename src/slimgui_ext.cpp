cc841c3df4ec8ec26fac2089071f418a62745e97
Janne Hellsten <jjhellst@gmail.com>
2025-03-09 18:26:14 +0200

    Add imgui.get_style_color_vec4

--- src/slimgui_ext.cpp
+++ src/slimgui_ext.cpp
@@ -419,7 +419,8 @@ NB_MODULE(slimgui_ext, m) {
     // IMGUI_API ImU32         GetColorU32(ImGuiCol idx, float alpha_mul = 1.0f);              // retrieve given style color with style alpha applied and optional extra alpha multiplier, packed as a 32-bit value suitable for ImDrawList
     // IMGUI_API ImU32         GetColorU32(const ImVec4& col);                                 // retrieve given color with style alpha applied, packed as a 32-bit value suitable for ImDrawList
     // IMGUI_API ImU32         GetColorU32(ImU32 col, float alpha_mul = 1.0f);                 // retrieve given color with style alpha applied, packed as a 32-bit value suitable for ImDrawList
-    // IMGUI_API const ImVec4& GetStyleColorVec4(ImGuiCol idx);                                // retrieve style color as stored in ImGuiStyle structure. use to feed back into PushStyleColor(), otherwise use GetColorU32() to get style color with style alpha baked in.
+    m.def("get_style_color_vec4", [](ImGuiCol_ idx) { return ImGui::GetStyleColorVec4(idx);}, "col"_a);
+
 
     // ID stack/scopes
     m.def("push_id", [](const char* str_id) {  ImGui::PushID(str_id); }, "str_id"_a);
@@ -477,16 +478,6 @@ NB_MODULE(slimgui_ext, m) {
         bool changed = ImGui::Combo(label, &current_item, &items[0], items.size(), popup_max_height_in_items);
         return std::tuple(changed, current_item);
     }, "label"_a, "current_item"_a, "items"_a, "popup_max_height_in_items"_a = -1);
-
-    // // Style read access
-    // // - Use the ShowStyleEditor() function to interactively see/edit the colors.
-    // IMGUI_API ImFont*       GetFont();                                                      // get current font
-    // IMGUI_API float         GetFontSize();                                                  // get current font size (= height in pixels) of current font with current scale applied
-    // IMGUI_API ImVec2        GetFontTexUvWhitePixel();                                       // get UV coordinate for a while pixel, useful to draw custom shapes via the ImDrawList API
-    // IMGUI_API ImU32         GetColorU32(ImGuiCol idx, float alpha_mul = 1.0f);              // retrieve given style color with style alpha applied and optional extra alpha multiplier, packed as a 32-bit value suitable for ImDrawList
-    // IMGUI_API ImU32         GetColorU32(const ImVec4& col);                                 // retrieve given color with style alpha applied, packed as a 32-bit value suitable for ImDrawList
-    // IMGUI_API ImU32         GetColorU32(ImU32 col, float alpha_mul = 1.0f);                 // retrieve given color with style alpha applied, packed as a 32-bit value suitable for ImDrawList
-    m.def("get_style_color_vec4", [](ImGuiCol_ idx) { return ImGui::GetStyleColorVec4(idx);}, "col"_a);
 
     m.def("get_cursor_screen_pos", &ImGui::GetCursorScreenPos);
     m.def("set_cursor_screen_pos", &ImGui::SetCursorScreenPos, "pos"_a);
