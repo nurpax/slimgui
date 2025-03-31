407d7d2ed1ae9f821ac2943b83e8b983ce0c03d0
Janne Hellsten <jjhellst@gmail.com>
2025-03-31 22:57:46 +0300

    more plotting funcs

--- example/implot_demo_window/implot.py
+++ example/implot_demo_window/implot.py
@@ -1,5 +1,8 @@
+import numpy as np
 from slimgui import imgui
 from slimgui import implot
+
+from slimgui.implot import LineFlags, PlotFlags, ScatterFlags
 
 def show_demo_window(show_window: bool):
     main_viewport = imgui.get_main_viewport()
@@ -10,6 +13,43 @@ def show_demo_window(show_window: bool):
     if not visible:
         imgui.end()
         return show_window
+
+    if imgui.collapsing_header('Basic line plots')[0]:
+        if implot.begin_plot("Plot", flags=PlotFlags.NO_LEGEND):
+            data_x = np.arange(0, 10, 0.25)
+            data_y = np.sin(data_x)
+            implot.plot_line("Sine Wave", data_y[::2]) # striding should work
+            implot.plot_line("Sine Wave 2", data_x, np.abs(data_y))
+            implot.end_plot()
+
+    if imgui.collapsing_header('Line and scatter')[0]:
+        if implot.begin_plot("Plot 2"):
+            data_x = np.arange(0, 10, 0.25)
+            data_y = np.sin(data_x)
+            implot.plot_line("Sine Wave", data_y)
+
+            # Generate a spiral pattern
+            theta = np.linspace(0, 4 * np.pi, 50)
+            r = np.linspace(0, 10, 50)
+            xs = r * np.cos(theta)
+            ys = r * np.sin(theta)
+            implot.plot_scatter("Scatter ys", ys)
+            implot.plot_scatter("Scatter xs, ys", xs, ys)
+
+            implot.end_plot()
+
+    if imgui.collapsing_header('Stairs##heading')[0]:
+        if implot.begin_plot("Stairs"):
+            x_steps = np.arange(0, 10, 1)
+            y_steps = np.repeat(np.arange(1, 11), 2)[1:-1]
+            implot.plot_stairs("Stairs", x_steps, y_steps)
+            implot.end_plot()
+
+        if implot.begin_plot("Shaded"):
+            x_steps = np.arange(0, 10, 1)
+            y_steps = np.repeat(np.arange(1, 11), 2)[1:-1]
+            implot.plot_shaded("Shaded", x_steps, y_steps, yref=2.3)
+            implot.end_plot()
 
     imgui.end()
     return show_window
--- src/implot_bindings.cpp
+++ src/implot_bindings.cpp
@@ -18,6 +18,8 @@
 namespace nb = nanobind;
 using namespace nb::literals;
 
+typedef nb::ndarray<const double, nb::ndim<1>, nb::device::cpu, nb::c_contig> ndarray_1d;
+
 void implot_bindings(nb::module_& m) {
     #include "implot_enums.inl"
 
@@ -33,6 +35,41 @@ void implot_bindings(nb::module_& m) {
         ImPlot::ShowDemoWindow(closable ? &open : nullptr);
         return open;
     }, "closable"_a = false);
+
+    // PlotLine functions
+    m.def("plot_line", [](const char* label_id, ndarray_1d& values, double xscale, double xstart, ImPlotLineFlags_ flags) {
+        ImPlot::PlotLine(label_id, (const double*)values.data(), values.shape(0), xscale, xstart, flags);
+    }, "label_id"_a, "values"_a, "xscale"_a = 1.0, "xstart"_a = 0.0, "flags"_a.sig("LineFlags.NONE") = ImPlotLineFlags_None);
+    m.def("plot_line", [](const char* label_id, ndarray_1d& xs, ndarray_1d& ys, ImPlotLineFlags_ flags) {
+        ImPlot::PlotLine(label_id, (const double*)xs.data(), (const double*)ys.data(), xs.shape(0), flags);
+    }, "label_id"_a, "xs"_a, "ys"_a, "flags"_a.sig("LineFlags.NONE") = ImPlotLineFlags_None);
+
+    // PlotScatter functions
+    m.def("plot_scatter", [](const char* label_id, ndarray_1d& values, double xscale, double xstart, ImPlotScatterFlags_ flags) {
+        ImPlot::PlotScatter(label_id, (const double*)values.data(), values.shape(0), xscale, xstart, flags);
+    }, "label_id"_a, "values"_a, "xscale"_a = 1.0, "xstart"_a = 0.0, "flags"_a.sig("ScatterFlags.NONE") = ImPlotScatterFlags_None);
+    m.def("plot_scatter", [](const char* label_id, ndarray_1d& xs, ndarray_1d& ys, ImPlotScatterFlags_ flags) {
+        ImPlot::PlotScatter(label_id, (const double*)xs.data(), (const double*)ys.data(), xs.shape(0), flags);
+    }, "label_id"_a, "xs"_a, "ys"_a, "flags"_a.sig("ScatterFlags.NONE") = ImPlotScatterFlags_None);
+
+    // PlotStairs functions
+    m.def("plot_stairs", [](const char* label_id, ndarray_1d& values, double xscale, double xstart, ImPlotStairsFlags_ flags) {
+        ImPlot::PlotStairs(label_id, (const double*)values.data(), values.shape(0), xscale, xstart, flags);
+    }, "label_id"_a, "values"_a, "xscale"_a = 1.0, "xstart"_a = 0.0, "flags"_a.sig("StairsFlags.NONE") = ImPlotStairsFlags_None);
+    m.def("plot_stairs", [](const char* label_id, ndarray_1d& xs, ndarray_1d& ys, ImPlotStairsFlags_ flags) {
+        ImPlot::PlotStairs(label_id, (const double*)xs.data(), (const double*)ys.data(), xs.shape(0), flags);
+    }, "label_id"_a, "xs"_a, "ys"_a, "flags"_a.sig("StairsFlags.NONE") = ImPlotStairsFlags_None);
+
+    // PlotShaded functions
+    m.def("plot_shaded", [](const char* label_id, ndarray_1d& values, double yref, double xscale, double xstart, ImPlotShadedFlags_ flags) {
+        ImPlot::PlotShaded(label_id, (const double*)values.data(), values.shape(0), yref, xscale, xstart, flags);
+    }, "label_id"_a, "values"_a, "yref"_a = 0, "xscale"_a = 1.0, "xstart"_a = 0.0, "flags"_a.sig("ShadedFlags.NONE") = ImPlotShadedFlags_None);
+    m.def("plot_shaded", [](const char* label_id, ndarray_1d& xs, ndarray_1d& ys, double yref, ImPlotShadedFlags_ flags) {
+        ImPlot::PlotShaded(label_id, (const double*)xs.data(), (const double*)ys.data(), xs.shape(0), yref, flags);
+    }, "label_id"_a, "xs"_a, "ys"_a, "yref"_a = 0, "flags"_a.sig("ShadedFlags.NONE") = ImPlotShadedFlags_None);
+    m.def("plot_shaded", [](const char* label_id, ndarray_1d& xs, ndarray_1d& ys1, ndarray_1d& ys2, ImPlotShadedFlags_ flags) {
+        ImPlot::PlotShaded(label_id, (const double*)xs.data(), (const double*)ys1.data(), (const double*)ys2.data(), xs.shape(0), flags);
+    }, "label_id"_a, "xs"_a, "ys1"_a, "ys2"_a, "flags"_a.sig("ShadedFlags.NONE") = ImPlotShadedFlags_None);
 
 #include "implot_funcs.inl"
 }
